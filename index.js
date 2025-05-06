b03c1f1729a4e7e9e6b042d0879481f0d4cfb762
Augment Chinedu <augmentchinedu@gmail.com>
2025-05-06 14:51:54 +0100

    GK Server Initialized

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const express = require("express");
+
+const app = express();
+
+// HTTPS Redirect
+app.use((req, res, next) => {
+	if (process.env.NODE_ENV == "production")
+		if (req.headers["x-forwarded-proto"] !== "https") {
+			return res.redirect(`https://${req.headers.host}${req.url}`);
+		}
+	next();
+});
+
+app.set("trust proxy", true);
+
+// Middlewares
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Catch All
+app.all("*", (_, res) => {
+	res.json({});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server Started @ ${PORT}`));
